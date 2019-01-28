6818dabc6212387c20ee4155f71c18483a1ea3fd
John P. Willis <jpw@geodigraph.com>
2019-01-28 08:21:23 -0700

    Add admin.js

---
+++ web/js/admin.js
@@ -0,0 +1,24 @@
+Prefiniti.extend("Admin", {
+	
+	beginAcctLookup: function() {
+        Prefiniti.dialog('/webware_admin/account_lookup.cfm');
+    },
+
+    locateAccount: function() {
+        $.get("/api/account/" + $("#acct-lookup-email").val(), (data) => {
+            if(!data.success) {
+            	alert("Error in account lookup.");
+            }
+            else {
+            	if(data.matches != 1) {
+            		alert("No account matches.");
+            	}
+            	else {
+            		let url = "/webware_admin/edit_account.cfm?id=" + data.account.id;
+            		Prefiniti.loadPage(url);
+            	}
+            }
+        });
+    }
+
+});
